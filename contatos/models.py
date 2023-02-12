9ddb2207cc033c04e874c3b628d78f8c6b1701da
Athus <athus.iasd@hotmail.com>
2023-02-12 17:49:54 -0300

    mais algumas evolucoes

--- contatos/models.py
+++ contatos/models.py
@@ -1,7 +1,6 @@
 from django.db import models
 from django.utils import timezone
 
-# Create your models here.
 
 class Categoria(models.Model):
     nome = models.CharField(max_length=255)
@@ -17,6 +16,7 @@ class Contato(models.Model):
     data_criacao = models.DateTimeField(default=timezone.now)
     descricao = models.TextField(blank=True)
     categoria = models.ForeignKey(Categoria, on_delete=models.DO_NOTHING)
+    mostrar = models.BooleanField(default=True)
     
     def __str__(self):
         return self.nome
